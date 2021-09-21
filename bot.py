99b7653be541abfda1b0224255f1066a87cfa1d3
Egor Tyuvaev <egor.tyuvaev@gmail.com>
2021-09-21 22:43:37 +0300

    Make async entrypoint

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 
 import os
+import asyncio
 
 from discord.ext import commands
 
@@ -12,13 +13,21 @@ if DISCORD_BOT_TOKEN is None:
     exit(-1)
 
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("%"),
-    description='Music Bot')
+async def main():
+    bot = commands.Bot(command_prefix=commands.when_mentioned_or("%"),
+        description='Music Bot')
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+        print('------')
 
-bot.add_cog(MusicPlayerCommands(bot))
-bot.run(DISCORD_BOT_TOKEN)
+    bot.add_cog(MusicPlayerCommands(bot))
+
+    try:
+        await bot.start(DISCORD_BOT_TOKEN)
+    finally:
+        if not bot.is_closed():
+            await bot.close()
+
+asyncio.get_event_loop().run_until_complete(main())
